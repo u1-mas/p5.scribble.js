8541c084645b593eea69b763508575dd26b985af
u1_mas <mtakuya1022@gmail.com>
2025-03-16 14:24:16 +0900

    [refactor] Viteの機能のみを使用するように修正 (from cline)

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,18 @@ import { defineConfig } from "vite";
 import { resolve } from "node:path";
 
 export default defineConfig(({ command }) => ({
-base: "/p5.scribble.js/",
-root: "examples",
-assetsInclude: ["**/*.js"],
-	build: {
-		outDir: resolve(__dirname, "dist"),
-		emptyOutDir: true,
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "examples", "index.html"),
-				chart: resolve(__dirname, "examples", "chart.html"),
-			},
-		},
-	},
-	publicDir: "public",
+  base: "/p5.scribble.js/",
+  root: "examples",
+  assetsInclude: ["**/*.js"],
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "examples/index.html"),
+        chart: resolve(__dirname, "examples/chart.html")
+      }
+    }
+  },
+  publicDir: "public"
 }));
